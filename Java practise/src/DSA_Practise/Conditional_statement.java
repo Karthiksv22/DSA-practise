77423d228bcd63e170d7de8a2ec64a8debc8f487
Karthik S V <ksv1844@gmail.com>
2025-03-31 23:44:16 +0530

    Started from scratch covered up variables and conditional statements

---
+++ Java practise/src/DSA_Practise/Conditional_statement.java
@@ -0,0 +1,13 @@
+package DSA_Practise;
+
+public class Conditional_statement {
+
+    public static void main(String[] args) {
+
+        int x;
+
+        if(x % 2){
+
+        }
+    }
+}
---
+++ Java practise/src/DSA_Practise/Variables.java
@@ -0,0 +1,26 @@
+package DSA_Practise;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        int sum =x +y;
+        System.out.println(" Sum Value "+sum);          // this is for variables for addition
+
+        int prd = x * y;
+        System.out.println(" PRod Value "+prd);
+
+        int v1 = x / y;
+        int v2 = y / x;
+        int v3 = x % y;
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+
+        //exp
+        int exp = x * y / x + y;
+        System.out.println(" Expression 1 "+exp);
+
+    }
+}
