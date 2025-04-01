9309cd8af50e09aa25c909640eb5dfc61cd2f3df
Karthik S V <ksv1844@gmail.com>
2025-04-02 00:20:42 +0530

    Commit -3 for condtional statements

--- Java practise/src/DSA_Practise/Conditional_statement.java
+++ Java practise/src/DSA_Practise/Conditional_statement.java
@@ -4,10 +4,15 @@ public class Conditional_statement {
 
     public static void main(String[] args) {
 
-        int x;
+        int x = 11;
 
-        if(x % 2){
-
+        if(x % 2 == 0){
+            System.out.println(x + " is even ");
         }
+        else
+        {
+            System.out.println(x + " is Odd ");
+        }
+        System.out.println("Hardwork is better than smart work");
     }
 }
