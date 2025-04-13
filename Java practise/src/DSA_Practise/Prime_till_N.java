a3eb59283a63ddb5729ab0a744330e65e5639305
Karthik S V <ksv1844@gmail.com>
2025-04-13 14:15:54 +0530

    solution for printing n number in prime

---
+++ Java practise/src/DSA_Practise/Prime_till_N.java
@@ -0,0 +1,27 @@
+package DSA_Practise;
+
+import java.util.Scanner;
+
+public class Prime_till_N {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int high = scn.nextInt();
+        int low = scn.nextInt();
+
+        for (int n = low; n <= high; n++){
+            int count = 0;
+
+            for(int div = 2; div*div <=n; div++){
+                if (n%div == 0){
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 0){
+                System.out.println(n);
+            }
+    }
+    }
+}
